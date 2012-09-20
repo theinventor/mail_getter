64bd2b09398d282ac413a4eb0cbe0a5611f1f5d7
Troy Anderson <Troy.Anderson@citygridmedia.com>
2012-09-20 11:05:54 -0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MailGetter::Application.config.secret_token = 'a359b0bef6aa117d15ca3fedda6e9a5af9bcd2f0d43534c761dae62448cabc91516dbf092e5e5d8fd2e2bcdeb1cc1c0af6f12337b40b7e9dfa790c6dd88eaf86'
